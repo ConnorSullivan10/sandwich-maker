d9e0f94d9848a9291b2e3a4be6c77418382702b9
Connor Sullivan <cksullvn@gmail.com>
2019-09-28 08:24:06 -0500

    created condiment.js and added all necessary functions to this file along with order.js and main.js so that the condiment radio buttons display and print to the final order card

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,6 @@
 import bread from './components/bread.js'
 import veggie from './components/veggie.js'
+import condiment from './components/condiment.js'
 import order from './components/order.js';
 //3rd thing: make init function in the main.js
 
@@ -7,6 +8,7 @@ const init = () => {
     //do stuff
     bread.printBreadOptions();
     veggie.printVeggieOptions();
+    condiment.printCondimentOptions();
     order.printOrderButton();
     //inside store there's a function call this and run it
 };
