da4873ba14a84afac72dc4e76b9c69d5386b6a45
Connor Sullivan <cksullvn@gmail.com>
2019-11-26 19:34:31 -0600

    updated final order to have close functionality and fixed problems indicated by linter

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import 'bootstrap';
 import '../styles/main.scss';
 import bread from './components/bread';
@@ -6,6 +7,20 @@ import condiment from './components/condiment';
 import cheese from './components/cheese';
 import meat from './components/meat';
 import sandwich from './components/sandwich';
+import utilities from './helpers/utilities';
+
+const closeSandwichOrder = () => {
+  $('body').on('click', '.close', () => {
+    utilities.printToDom('final-order', '');
+    bread.printBreadOptions();
+    veggie.printVeggieOptions();
+    condiment.printCondimentOptions();
+    cheese.printCheeseOptions();
+    meat.printMeatOptions();
+    sandwich.printOrderButton();
+  });
+};
+
 
 const init = () => {
   bread.printBreadOptions();
@@ -14,6 +29,7 @@ const init = () => {
   cheese.printCheeseOptions();
   meat.printMeatOptions();
   sandwich.printOrderButton();
+  closeSandwichOrder();
 };
 
 init();
